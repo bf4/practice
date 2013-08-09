e69bc0dc2066219fff3bdf34f101fdc3c134da60
Benjamin Fleischer <github@benjaminfleischer.com>
2013-08-08 19:06:43 -0500

    No more bob, please

--- exercism/ruby/bob/bob.rb
+++ exercism/ruby/bob/bob.rb
@@ -1,61 +1,39 @@
 class Bob
 
-  # @param msg [String] The message Bob is responding to
-  # @return response [String] Bob's response to the message
-  def hey(msg)
-    case message_type(msg)
-    when :silent   then "Fine. Be that way!"
-    when :shout    then "Woah, chill out!"
-    when :question then "Sure."
-    else                "Whatever."
+  attr_reader :drivel
+
+  # @param drivel [String] The drivel Bob is responding to
+  # @return response [String] Bob's response to the drivel
+  def hey(drivel)
+    @drivel = drivel
+    case
+    when silence?   then "Fine. Be that way!"
+    when shouting?  then "Woah, chill out!"
+    when asking?    then "Sure."
+    else                 "Whatever."
     end
   end
 
   private
 
-  def message_type(msg)
-    MessageType.new(msg).message_type
+  def shouting?
+    drivel.upcase == drivel
   end
 
-  # Determines the message_type for a given message
-  # @param msg [String]
-  MessageType = Struct.new(:msg) do
+  def stating?
+    drivel.end_with?('.')
+  end
 
-    # @return message_type [Symbol]
-    def message_type
-      case
-      when silence?   then :silent
-      when shouting?  then :shout
-      when asking?    then :question
-      when stating?   then :statement
-      when demanding? then :demand
-      else
-        STDERR.puts "Could not determine message type for #{msg}"
-      end
-    end
+  def asking?
+    drivel.end_with?('?')
+  end
 
-    private
+  def demanding?
+    drivel.end_with?('!')
+  end
 
-    def shouting?
-      msg.upcase == msg
-    end
-
-    def stating?
-      msg.end_with?('.')
-    end
-
-    def asking?
-      msg.end_with?('?')
-    end
-
-    def demanding?
-      msg.end_with?('!')
-    end
-
-    def silence?
-      msg.nil? || msg.strip == ''
-    end
-
+  def silence?
+    drivel.nil? || drivel.strip == ''
   end
 
 end
