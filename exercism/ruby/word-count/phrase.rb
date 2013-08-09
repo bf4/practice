f271bc5feadcfa2493b4a7bba6a484892454ef00
Benjamin Fleischer <github@benjaminfleischer.com>
2013-08-09 12:19:11 -0500

    A WordCounter splits a sentance into words and can count them up

--- exercism/ruby/word-count/phrase.rb
+++ exercism/ruby/word-count/phrase.rb
@@ -2,22 +2,30 @@ class Phrase
 
   def initialize(word)
     @word = word
-    @result = Hash.new {|hash,k| hash[k] = 0}
   end
 
   def word_count
-    words.each do |word|
-      @result[word.downcase] += 1
+    WordCounter.new(@word).word_count
+  end
+
+end
+
+WordCounter = Struct.new(:sentance) do
+
+  def words
+    @words ||= sentance.split(/[^A-Za-z0-9]/).reject(&:empty?)
+  end
+
+  def word_count
+    words.each_with_object(hash_with_default(0)) do |word, counter|
+      counter[word.downcase] += 1
     end
-    @word = ''
-    @result
   end
 
   private
 
-  def words
-    @word.split(/[^A-Za-z0-9]/).reject(&:empty?)
+  def hash_with_default(value)
+    Hash.new {|hash,k| hash[k] = value}
   end
-
 
 end
