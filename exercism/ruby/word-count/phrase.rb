033277ed619e04648e64aed85aee7a1b6697d411
Benjamin Fleischer <github@benjaminfleischer.com>
2013-08-09 11:54:13 -0500

    All tests passing, time to refactor

--- exercism/ruby/word-count/phrase.rb
+++ exercism/ruby/word-count/phrase.rb
@@ -2,10 +2,15 @@ class Phrase
 
   def initialize(word)
     @word = word
+    @result = Hash.new {|hash,k| hash[k] = 0}
   end
 
   def word_count
-    {@word => 1}
+    @word.split(/[^A-Za-z0-9]/).each do |word|
+      @result[word.downcase] += 1 unless word.empty?
+    end
+    @word = ''
+    @result
   end
 
 end
