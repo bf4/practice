4d08725f51083dbc6cbdd75a9ed308458018f7af
Benjamin Fleischer <github@benjaminfleischer.com>
2015-05-15 10:26:47 -0500

    Use return value

--- exercism/go/leap/leap.go
+++ exercism/go/leap/leap.go
@@ -4,14 +4,15 @@ import (
 	"math"
 )
 
-func IsLeapYear(year int) bool {
+func IsLeapYear(year int) (answer bool) {
 	year64 := float64(year)
+	answer = false
 	if math.Mod(year64, 4.0) == 0 {
 		if math.Mod(year64, 100.0) == 0 {
-			return math.Mod(year64, 400.0) == 0
+			answer = math.Mod(year64, 400.0) == 0
 		} else {
-			return true
+			answer = true
 		}
 	}
-	return false
+	return answer
 }
