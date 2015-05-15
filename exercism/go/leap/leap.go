2444dfd76ceadd54429f8ad060ecdc7e22fb05ed
Benjamin Fleischer <github@benjaminfleischer.com>
2015-05-15 10:30:23 -0500

    Factor out functions

--- exercism/go/leap/leap.go
+++ exercism/go/leap/leap.go
@@ -7,12 +7,24 @@ import (
 func IsLeapYear(year int) (answer bool) {
 	year64 := float64(year)
 	answer = false
-	if math.Mod(year64, 4.0) == 0 {
-		if math.Mod(year64, 100.0) == 0 {
-			answer = math.Mod(year64, 400.0) == 0
+	if isVanillaLeapYear(year64) {
+		if isCentury(year64) {
+			answer = isExceptionalCentury(year64)
 		} else {
 			answer = true
 		}
 	}
 	return answer
+}
+
+func isVanillaLeapYear(year64 float64) bool {
+	return math.Mod(year64, 4.0) == 0
+}
+
+func isCentury(year64 float64) bool {
+	return math.Mod(year64, 100.0) == 0
+}
+
+func isExceptionalCentury(year64 float64) bool {
+	return math.Mod(year64, 400.0) == 0
 }
