a0c532886267c8e8693a771f2e2ce8d268fef29b
Benjamin Fleischer <github@benjaminfleischer.com>
2015-10-22 21:14:51 -0500

    Katrina

--- exercism/go/leap/leap.go
+++ exercism/go/leap/leap.go
@@ -1,26 +1,20 @@
+// Package leap contains logic to determine leap years.
 package leap
 
-import (
-	"math"
-)
+// TestVersion is the version of the test suite that
+// the solution references.
+const TestVersion = 1
 
-func IsLeapYear(year int) bool {
-	year64 := float64(year)
-	if isCentury(year64) {
-		return isExceptionalCentury(year64)
-	} else {
-		return isVanillaLeapYear(year64)
+// IsLeapYear determines whether or not the given year is a leap year.
+// This is only valid for the Gregorian calendar.
+// While the algorithm works for years that occurred before the Gregorian reform
+// in 1582, it is not valid for negative years (representing BC notation).
+//
+// Check out this delightful 4 minute video by CGP Gray that explains why we have leap years.
+// https://www.youtube.com/watch?v=xX96xng7sAE
+func IsLeapYear(y int) bool {
+	divBy := func(n int) bool {
+		return y%n == 0
 	}
-}
-
-func isVanillaLeapYear(year64 float64) bool {
-	return math.Mod(year64, 4.0) == 0
-}
-
-func isCentury(year64 float64) bool {
-	return math.Mod(year64, 100.0) == 0
-}
-
-func isExceptionalCentury(year64 float64) bool {
-	return math.Mod(year64, 400.0) == 0
+	return divBy(4) && !divBy(100) || divBy(400)
 }
